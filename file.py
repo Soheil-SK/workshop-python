34e147c9687c136708d0b6eb07b3a6a367089475
Khalil Soheil <soheil.khalil@epitech.eu>
2022-06-16 16:54:34 +0200

    add: decorator not workunbg

---
+++ file.py
@@ -0,0 +1,28 @@
+
+i = 0
+for i in range(0, 100):
+    if (i % 3 == 0):
+        print('Three')
+    elif (i % 5 == 0):
+        print('Five')
+    else:
+        print(i)
+
+def calculate(a):
+    result = 0;
+    if (type(a) 
+    for i in a:
+        if (i.isstring()):
+            result += i
+    return (result);
+        
+calculate(['nothing', 3, '8', 2, '1'])
+
+def decorator_to_str(func):
+        def inner (*args):
+        return (str(func(*args))
+                return inner
+                @decorator_to_str
+                def add(a, b):
+                @decorator_to_str
+                def_get_info()
